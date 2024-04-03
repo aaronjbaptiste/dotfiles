00b63b7ea9d921e55b85f982077fc6b06b6b147d
Aaron John-Baptiste <gravityapple@gmail.com>
2024-04-04 00:15:19 +0100

    initial config

---
+++ README.md
@@ -0,0 +1,23 @@
+# Dotfiles
+
+Full MacOS system setup using [Nix](https://github.com/NixOS/nix) and [Nix-Darwin](https://github.com/LnL7/nix-darwin)
+
+## Installation
+
+Run at your own risk, please review the source before running random scripts:
+
+```sh
+curl -fsSL https://raw.githubusercontent.com/aaronjbaptiste/dotfiles/main/install | bash
+```
+
+## What does this do?
+
+- [x] Installs Command Line Tools silently
+- [x] Installs [Nix](https://github.com/NixOS/nix)
+- [x] Installs [Nix-Darwin](https://github.com/LnL7/nix-darwin)
+- [x] Clones this repo
+- [x] Initiates the setup
+
+## Credits
+
+[matchai](https://github.com/matchai/dotfiles)
---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Aarons configuration for MacOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    darwin = {
+      url = "github:LnL7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, darwin, nixpkgs } @inputs:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
+        ];
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = false;
+    };
+  in
+  {
+    darwinConfigurations = {
+      macbook = darwin.lib.darwinSystem {
+        modules = [ configuration ];
+      };
+    };
+
+  };
+}

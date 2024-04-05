ce5cffe30b52b2f0ac2b30cf333394c66c1a0d88
Aaron John-Baptiste <gravityapple@gmail.com>
2024-04-05 01:00:23 +0100

    add dock settings

--- hosts/sixstrings/default.nix
+++ hosts/sixstrings/default.nix
@@ -25,6 +25,36 @@
 
   system.stateVersion = 4;
 
+  system.defaults.dock = {
+    appswitcher-all-displays = true;
+    autohide = true;
+    autohide-delay = 0.0;
+    autohide-time-modifier = 0.15;
+    dashboard-in-overlay = false;
+    enable-spring-load-actions-on-all-items = false;
+    expose-animation-duration = 0.2;
+    expose-group-by-app = false;
+    launchanim = true;
+    mineffect = "genie";
+    minimize-to-application = false;
+    mouse-over-hilite-stack = true;
+    mru-spaces = false;
+    orientation = "bottom";
+    show-process-indicators = true;
+    show-recents = true;
+    showhidden = true;
+    static-only = false;
+    tilesize = 48;
+    wvous-bl-corner = 1;
+    wvous-br-corner = 1;
+    wvous-tl-corner = 1;
+    wvous-tr-corner = 1;
+    persistent-apps = [
+      "/Applications/Nix Apps/Visual Studio Code.app"
+      "/Applications/Rider.app"
+    ];
+  };
+
   nixpkgs.hostPlatform = "aarch64-darwin";
   nixpkgs.config.allowUnfree = true;
 }

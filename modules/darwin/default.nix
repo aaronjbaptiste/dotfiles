69009845b7b897a06741d4767413e49d712ca96b
Aaron John-Baptiste <gravityapple@gmail.com>
2024-04-06 02:48:51 +0100

    another setting?!?

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -5,7 +5,8 @@
     system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
     system.defaults.NSGlobalDomain."com.apple.trackpad.trackpadCornerClickBehavior" = 1;
     system.defaults.trackpad.TrackpadRightClick = true;
-    system.defaults.trackpad.Clicking = 1;
+    system.defaults.trackpad.Clicking = true;
+    system.defaults.trackpad.TrackpadRightClick = true;
     system.defaults.NSGlobalDomain."com.apple.trackpad.enableSecondaryClick" = true;
 
     homebrew = {

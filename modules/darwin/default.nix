2cb31a70d3cbe51bffa42e1a4f7881c05050efeb
Aaron John-Baptiste <gravityapple@gmail.com>
2024-04-06 02:45:08 +0100

    trying other settings

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -3,8 +3,10 @@
 {
     system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = false;
     system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
+    system.defaults.NSGlobalDomain."com.apple.trackpad.trackpadCornerClickBehavior" = 1;
+    system.defaults.trackpad.TrackpadRightClick = true;
+    system.defaults.trackpad.Clicking = 1;
     system.defaults.NSGlobalDomain."com.apple.trackpad.enableSecondaryClick" = true;
-    system.defaults.NSGlobalDomain."com.apple.trackpad.trackpadCornerClickBehavior" = 2;
 
     homebrew = {
         enable = true;

51d5b3074843b01ac66517b71343e6125fe93b3e
Aaron John-Baptiste <gravityapple@gmail.com>
2025-10-24 23:48:05 +0100

    fix: npm not working on bootstrap

--- .config/yadm/bootstrap.d/npm.sh
+++ .config/yadm/bootstrap.d/npm.sh
@@ -2,6 +2,11 @@
 
 echo "[Start] Installing apps with npm"
 
+# Activate proto to make npm available
+if [ -f "$HOME/.proto/shims/npm" ]; then
+  export PATH="$HOME/.proto/shims:$PATH"
+fi
+
 npm install -g @anthropic-ai/claude-code
 npm install -g @openai/codex
 

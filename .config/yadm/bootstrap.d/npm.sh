8ddb0eb317eb3d874cdd155b447c8de3de499784
Aaron John-Baptiste <gravityapple@gmail.com>
2025-10-31 08:59:59 +0000

    add emmet lsp to helix

--- .config/yadm/bootstrap.d/npm.sh
+++ .config/yadm/bootstrap.d/npm.sh
@@ -4,5 +4,6 @@ echo "[Start] Installing apps with npm"
 
 npm install -g @anthropic-ai/claude-code
 npm install -g @openai/codex
+npm install -g @olrtg/emmet-language-server
 
 echo "[Done] Installing apps with npm"

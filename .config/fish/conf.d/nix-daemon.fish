ed0f19a0e36ba7263686994c99e75200ce6f7da9
Aaron John-Baptiste <aaronjb@canva.com>
2026-09-07 11:11:39 +0100

    add workstation fish fix

---
+++ .config/fish/conf.d/nix-daemon.fish
@@ -0,0 +1,4 @@
+# Fixes fish shell on work machine
+if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+    source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+end
